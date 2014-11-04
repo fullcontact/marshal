710285bcdf7dcb811491bb87b4e0bbd752233027
Brandon Vargo <brandon.vargo@gmail.com>
2014-11-04 11:43:46 -0700

    MarshalException: Make checked.

--- marshal/src/main/java/com/fullcontact/marshal/MarshalException.java
+++ marshal/src/main/java/com/fullcontact/marshal/MarshalException.java
@@ -1,12 +1,14 @@
 package com.fullcontact.marshal;
 
+import java.io.IOException;
+
 /**
  * An exception for use by a Marshal. Other classes can also use this exception to indicate
  * marshaling or demarshaling errors when using Marshals.
  *
  * @author Brandon Vargo
  */
-public class MarshalException extends RuntimeException {
+public class MarshalException extends IOException {
     public MarshalException(String message) {
         super(message);
     }
